080f6717412b7ed37572b6f49c3d29f17a43d443
KamijouMikoto12311 <2553032814@qq.com>
2025-02-09 22:36:13 +0800

    new file:   .vscode/c_cpp_properties.json

--- lecture02/main.cpp
+++ lecture02/main.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 
 using Zeros = std::pair<double, double>;
-using Solution = std::pair<bool, Zeros>; 
+using Solution = std::pair<bool, Zeros>;
 
 /**
  * Solves the equation ax^2 + bx + c = 0
@@ -18,25 +18,33 @@ Solution solveQuadratic(double a, double b, double c)
 {
   // Your code here...
   double discrim = b * b - 4 * a * c;
-  if (discrim < 0) return { false, { 106, 106 }};
+  if (discrim < 0)
+    return {false, {106, 106}};
 
   double root = sqrt(discrim);
-  return { true, { (-b - root) / (2 * a), (-b + root) / (2 * a) }};
+  return {true, {(-b - root) / (2 * a), (-b + root) / (2 * a)}};
 }
 
-int main() {
+int main()
+{
   // Get the values for a, b, and c from the user
   double a, b, c;
-  std::cout << "a: "; std::cin >> a;
-  std::cout << "b: "; std::cin >> b;
-  std::cout << "c: "; std::cin >> c;
+  std::cout << "a: ";
+  std::cin >> a;
+  std::cout << "b: ";
+  std::cin >> b;
+  std::cout << "c: ";
+  std::cin >> c;
 
   // Solve the quadratic equation, using our quadratic function above
   auto result = solveQuadratic(a, b, c);
-  if (result.first) {
+  if (result.first)
+  {
     auto solutions = result.second;
     std::cout << "Solutions: " << solutions.first << ", " << solutions.second << std::endl;
-  } else {
+  }
+  else
+  {
     std::cout << "No solutions" << std::endl;
   }
 
